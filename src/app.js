041af0323716f9dee7b164f3be1ac11610183bba
Marina Gallardo <marinagallardo@gmail.com>
2023-01-31 11:32:35 +0100

    responsive and effect update

--- src/app.js
+++ src/app.js
@@ -10,17 +10,18 @@ gsap.registerPlugin(CustomEase);
 CustomEase.create('custom', '1,0,0,1')
 
 const animateReveal = (el) => {
-  gsap.fromTo(el, { y: "20%" }, { y: "0%", duration: 1.5, ease: 'power4.out' });
-  gsap.fromTo(el, { height: "0" }, { height: "50vh", duration: 1.5, ease: 'power4.out', delay: 0.3 });
-  gsap.fromTo(el, { opacity: "0" }, { opacity: "1", duration: 1.5, ease: 'power4.out', delay: 0.3 });
+  gsap.timeline()
+    .fromTo(el, { y: "100%" }, { y: "0%", duration: 2, ease: 'custom' })
+    .fromTo(el, { height: "100vh" }, { height: "50vh", duration: 2, ease: 'power4.out' });
 };
 
 const animateContainer = (el) => {
-  gsap.fromTo(el, { y: "5%" }, { y: "0%", duration: 1.5, delay: 0.2, ease: 'power4.out'})
+  gsap.fromTo(el, { opacity: "0" }, { opacity: "1", duration: 2, delay: 1, ease: 'power4.out'})
+  gsap.fromTo(el, { y: "20%" }, { y: "0%", duration: 2, delay: 0.7, ease: 'power4.out'})
 };
 
 const animateScaleImage = (el) => {
-  gsap.fromTo(el, { scale: "1" }, { scale: "1.3", duration: 3, ease: 'power4.out'})
+  gsap.fromTo(el, { scale: "1" }, { scale: "1.3", duration: 3, delay: 0.8, ease: 'power4.out'})
 };
 
 const animateContent = (el) => {
